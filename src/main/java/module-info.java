97ae6da660042fc290f684e9a75399bfff1f17c5
Meteoroid_Kun <31439634+0313johnny@users.noreply.github.com>
2022-10-27 21:38:52 +0800

    修正pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,17 +6,6 @@ module com.example.los {
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
     requires org.json;
-    requires fastjson;
-    requires io.netty.transport;
-    requires io.netty.codec;
-    requires google.oauth.client;
-    requires com.google.api.client.extensions.java6.auth;
-    requires com.google.api.client.extensions.jetty.auth;
-    requires google.api.client;
-    requires com.google.api.client;
-    requires com.google.api.client.json.jackson2;
-    requires guava;
-    requires google.api.services.youtube.v3.rev222;
 
     opens los to javafx.fxml;
     exports los;
